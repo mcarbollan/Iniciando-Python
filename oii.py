3602d740b7d4d050090cc398977003d0c3efb35b
Mariana Dreset Carbollan <mcarbollan@gmail.com>
2026-04-23 13:22:51 -0300

    Add files via upload

---
+++ oii.py
@@ -0,0 +1,12 @@
+print ("Hello World")
+nome = input("Insira seu nome")
+print ("Bom dia" + nome)
+
+idade = input("Digite sua idade")
+print (nome + " tem" + idade + " anos")
+
+profissão = input("Digite sua profissão")
+print (nome + " possui" + idade + " anos, e trabalha como" + profissão)
+
+o_que_procura = input("O que você procura")
+print (nome + " procura" + o_que_procura)
